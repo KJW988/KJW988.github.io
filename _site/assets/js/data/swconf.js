a16823cddd4a5ef9f13019d4ab56ac9fb571f79b
Kim Jiwon <livelim313@gmail.com>
2024-12-13 20:58:02 +0900

    Upgrade Ruby version

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734088574',resources: [
+    cacheName: 'chirpy-1734090674',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
