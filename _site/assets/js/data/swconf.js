86acacd070de4e7b40bc081e709d85f423c8dbcd
Kim Jiwon <livelim313@gmail.com>
2024-12-17 03:52:44 +0900

    Update font-family

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734090674',resources: [
+    cacheName: 'chirpy-1734375131',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
