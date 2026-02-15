34f7304a1319463b235d9162802ff0ae6356f815
Kim Jiwon <livelim313@gmail.com>
2026-02-16 03:33:06 +0900

    Post: 4th semester goal

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1771175728',resources: [
+    cacheName: 'chirpy-1771180351',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
